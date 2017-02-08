863113959ea8ccb51632f4311ec65c6343ce1417
Rustem Muslimov <r.muslimov@gmail.com>
2017-02-08 01:05:13 -0800

    Fixes in description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject eptash/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Custom lein template for web application with clj/cljs."
+  :url "http://github.com/rmuslimov/eptashx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
