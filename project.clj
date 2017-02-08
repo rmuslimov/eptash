5c22a4f654e3bae8582a04160eb4294e4855bc80
Rustem Muslimov <r.muslimov@gmail.com>
2017-02-08 01:11:20 -0800

    Fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eptash/lein-template "0.1.0-SNAPSHOT"
+(defproject eptash/lein-template "0.1.1"
   :description "Custom lein template for web application with clj/cljs."
   :url "http://github.com/rmuslimov/eptashx"
   :license {:name "Eclipse Public License"
